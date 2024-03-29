42268fef1c62a6e187e918cf7baf1a7aed128823
r3dotstone <jstone.import@gmail.com>
2024-03-29 09:23:01 -0400

    now with vid file

--- python/fuckabout.py
+++ python/fuckabout.py
@@ -1,18 +1,25 @@
 import numpy as np
 import cv2 as cv
- 
-input_file = '../vids/sprayTest1.mp4'
+import os 
+
+# os.chdir("../vids") 
+
+input_file = 'MilkPour.avi'
 cap = cv.VideoCapture(input_file)
- 
+
 while cap.isOpened():
     ret, frame = cap.read()
- 
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
+    #################################
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
- 
+    
+    
+    
+    
+    #################################
     cv.imshow('frame', gray)
     if cv.waitKey(25) == ord('q'):
         break
