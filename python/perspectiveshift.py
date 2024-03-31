112de44981f5744e7b62e019cc15bf37cadbce93
r3dotstone <jstone.import@gmail.com>
2024-03-31 18:38:13 -0400

    perspective shift, now working

--- python/perspectiveshift.py
+++ python/perspectiveshift.py
@@ -1,34 +1,40 @@
 import cv2 as cv
+import numpy as np
+import imutils
+import os
 
-inputFile = "stablePaintFrame.jpg"
-outputFile = "stablePaintFrameOUT.jpg"
+# https://pixspy.com/
+inputFile = "stablePaintFrame.png"
+outputFile = "stablePaintFrameOUT.png"
 save = False
 
-filePath = os.path.realpath(__file__)
-fileDir = os.path.dirname(filePath)
-imgDir = fileDir.replace('SPRRAE\python', 'pics')
-input_path = os.path.join(imgDir,inputFile)
-img = cv2.imread(inputFile)
+# filePath = os.path.realpath(__file__)
+# fileDir = os.path.dirname(filePath)
+# imgDir = fileDir.replace('SPRRAE\python', 'pics')
+# input_path = os.path.join(imgDir,inputFile)
+img = cv.imread(inputFile)
 
 # Coordinates that you want to Perspective Transform
-startPts = np.float32([[219,209],[612,8],[380,493],[785,271]])
+startPts = np.float32([[120,70],[1013,67],[50,650],[1117,644]])
 # Size of the Transformed Image
-endPts = np.float32([[0,0],[500,0],[0,400],[500,400]])
+endPts = np.float32([[0,0],[1280,0],[0,720],[1280,720]])
 # put some little circles on there
-for val in startPts:
-    cv.circle(img,(val[0],val[1]),5,(0,255,0),-1)
+# for val in startPts:
+#     cv.circle(img,(val[0],val[1]),5,(0,255,0),-1)
 # transformation matrix
 M = cv.getPerspectiveTransform(startPts,endPts)
 # transformed image
-dst = cv.warpPerspective(img,M,(500,400))
+dst = cv.warpPerspective(img,M,(1280,720))
 # before/after
 cat = np.concatenate((img, dst), axis=1)
-#show results
-cv.imshow(cat)
+cat = imutils.resize(cat, width = 600)
 
+#show results
+cv.imshow("warp",cat)
+cv.waitKey(0)
 #stuff for saving
-if save:
-    cv.imwrite(outputFile,cat)
+# if save:
+#     cv.imwrite(os.path.join(imgDir,outputFile),cat)
 
 # Closes all the frames
 cv.destroyAllWindows()
