d050c2214d1f4bc96ca323b9dfb5ab8ccfe2bed1
Jaden <jstone.import@gmail.com>
2024-05-03 12:51:51 -0400

    return position from controller

--- python/theController.py
+++ python/theController.py
@@ -6,7 +6,7 @@ class theControllerClass:
         self.kp_omega = 0.01
         self.ki_omega = 0.001
         self.dt = 0 #[milliseconds] <------PROBLEM???
-    
+
     def control(self, elapsedTime, refAngle, measuredAngle):
         omega = 0.1 #[deg/s]
         #measuredAngle = 0 #need to figure out how we're getting this from the video
@@ -18,11 +18,13 @@ class theControllerClass:
             omega = 0.1 + self.kp_omega * e_omega + self.ki_omega * ei_omega #
             # print(omega)
             time.sleep(0.3) #delay 1/3 second <------PROBLEM???
-            return omega
+            dAngle = (360 * omega * self.dt) / (2 * np.pi)
+            angle += dAngle
+            return angle
 
     def ref(self, elapsedTime): #WORKING!!!!
          #step function velocity vs time
-         #the period is 60s for 180 deg rotation 
+         #the period is 60s for 180 deg rotation
         rotVelocity = 3 #[deg/s]
         period = 30 #[s] for 90 degrees, 60 seconds for 180 degerees
         refAngle = 0
